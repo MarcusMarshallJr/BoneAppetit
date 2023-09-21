73056824e0ff2e50340b84d47ba05489762c8855
Marcus Marshall <marcus.marshall@imbearyhungry.com>
2023-09-21 15:19:20 -0500

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Marcus Marshall, Jr on 9/21/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
